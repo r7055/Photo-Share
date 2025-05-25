fffc2bcdd5e9b59d056e2db54a5b1f96405b2daa
hitmachut <hitmachut@students.local>
2025-05-25 22:17:20 +0300

    app setting to render

--- PhotoShareManager/src/environments/environment.ts
+++ PhotoShareManager/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:5141",
+  // apiUrl: "http://localhost:5141",
+  apiUrl: "https://photo-share-api-5sze.onrender.com",
 }
